c6fd1c0c179ab0d3a681349480d38236c9a80436
irene bernardi <irenebernardi22@gmail.com>
2023-05-22 11:11:09 +0200

    scaled density from 0.05 to 0.2 to execute

--- model/cfg_demo.py
+++ model/cfg_demo.py
@@ -120,7 +120,7 @@ cfg.scale = 1.0
 cfg.sizeY = 2000.0 
 cfg.sizeX = 200.0   
 cfg.sizeZ = 200.0
-cfg.scaleDensity = 0.05 # Should be 1.0 unless need lower cell density for test simulation or visualization
+cfg.scaleDensity = 0.20 # Should be 1.0 unless need lower cell density for test simulation or visualization
 
 
 #------------------------------------------------------------------------------
